588cc5924ee063e649f155fa56b0effd111c31af
Sónia Machado <scsm@ua.pt>
2019-02-04 16:47:07 +0000

    Merge remote-tracking branch 'relockers/master'
    # Conflicts:
    #	src/components/Inicio.js
    #	src/components/Navbar.js
    #	src/css/Geral.css
    #	src/css/Inicio.css

--- src/components/EncomendasEntregues.js
+++ src/components/EncomendasEntregues.js
@@ -46,29 +46,65 @@ export default class EncomendasEntregues extends React.Component {
                 </thead>
                 <tbody>
 
+
                 {encomendas.map(encomenda => {
 
-                    const {id, temperatura, tamanho, localizacao, tempolimite_de_levantamento, cliente, cacifo} = encomenda;
 
-                    const data_split = tempolimite_de_levantamento.split(" ");
-                    const end = moment(data_split[0]); //todays date
-                    const now = moment("1971-04-06"); // another date
+                    const {id, temperatura, tamanho, localizacao, tempo_limite_de_levantamento, data_de_levantamento, data_de_entrega_pretendida, data_de_entrega, cliente, cacifo} = encomenda;
+
+                    const data_entrega = data_de_entrega_pretendida.split(" ");
+
+                    const m = new Date();
+                    const new_timestamp =
+                        m.getUTCFullYear() + "-" +
+                        ("0" + (m.getUTCMonth() + 1)).slice(-2) + "-" +
+                        ("0" + m.getUTCDate()).slice(-2) + " " +
+                        ("0" + m.getUTCHours()).slice(-2) + ":" +
+                        ("0" + m.getUTCMinutes()).slice(-2) + ":" +
+                        ("0" + m.getUTCSeconds()).slice(-2);
+                    const prazo_levantamento = new_timestamp.split(" ");
+
+
+                    const data_split = tempo_limite_de_levantamento.split(" ");
+                    const now = moment(data_split[0]); //todays date
+                    const end = moment(prazo_levantamento[0]); // another date
                     const duration = moment.duration(now.diff(end));
                     let days = duration.asDays();
 
-                    if(days <= 0){
+                    if (days <= 0) {
                         days =
                             <Alert color="danger">
-                                Prazo excecido
+                                Prazo excedido
                             </Alert>
+                    }
+                    else if (days > 0) {
+                        days =
+                            <Alert color="success">
+                                Levantamento em {days} dias
+                            </Alert>;
+
+                            if (data_de_levantamento !=null){
+                                days=
+                                <Alert color="success">
+                                    Encomenda levantada
+                                </Alert>;
+                            }
+
+                    }
+
+
+                    if (data_de_entrega == null) {
+                        return (
+                            <div></div>
+                        );
                     }
 
 
                     return (
                         <tr key={id}>
                             <th scope="row">{id}</th>
-                            <td>{data_split[0]}</td>
-                            <td>{data_split[1]}</td>
+                            <td>{data_entrega[0]}</td>
+                            <td>{data_entrega[1]}</td>
                             <td>{localizacao}</td>
                             <td>{temperatura}ºC</td>
                             <td>{tamanho}</td>
@@ -81,7 +117,8 @@ export default class EncomendasEntregues extends React.Component {
                             <td>{days}</td>
                             <td>
                                 <span className="dropdown">
-				                        <button id="btnSearchDrop2" style={{backgroundColor: '#b5a0fb', border: 'none', width:'68px'}}
+				                        <button id="btnSearchDrop2"
+                                                style={{backgroundColor: '#b5a0fb', border: 'none', width: '68px'}}
                                                 type="button" data-toggle="dropdown" aria-haspopup="true"
                                                 aria-expanded="false"
                                                 className="btn btn-dark dropdown-toggle dropdown-menu-right">
@@ -93,9 +130,13 @@ export default class EncomendasEntregues extends React.Component {
                                         </button>
 				                        <span aria-labelledby="btnSearchDrop2"
                                               className="btn_acoes dropdown-menu mt-1 dropdown-menu-right">
-				                            <Link to={{pathname: `detail/${id}`, query: {id: id}}} className="dropdown-item"> <i className="material-icons md-18 icon">remove_red_eye</i> Abrir</Link>
-				                            <Link to="#" className="dropdown-item"><i className="material-icons md-18 icon">create</i> Editar</Link>
-				                            <Link to="#" className="dropdown-item"><i className="material-icons md-18 icon">delete</i> Remover</Link>
+				                            <Link to={{pathname: `detail/${id}`, query: {id: id}}}
+                                                  className="dropdown-item"> <i
+                                                className="material-icons md-18 icon">remove_red_eye</i> Abrir</Link>
+				                            <Link to="#" className="dropdown-item"><i
+                                                className="material-icons md-18 icon">create</i> Editar</Link>
+				                            <Link to="#" className="dropdown-item"><i
+                                                className="material-icons md-18 icon">delete</i> Remover</Link>
 				                        </span>
 				                    </span>
                             </td>
--- src/components/EncomendasPorEntregar.js
+++ src/components/EncomendasPorEntregar.js
@@ -1,87 +1,141 @@
 import React from 'react';
+import '../css/Encomendas.css'
+import {Alert, Table} from "reactstrap";
+import axios from "axios/index";
+import moment from "moment";
+import {Link} from "react-router-dom";
 
-import {Table} from "reactstrap";
 
 export default class EncomendasPorEntregar extends React.Component {
+    state = {
+        encomendas: [],
+        estafetas: []
+    };
+
+    componentDidMount() {
+        axios.get('http://167.99.202.225/api/encomendas')
+            .then(response => {
+                this.setState({encomendas: response.data.data});
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
 
     render() {
+        const encomendas = this.state.encomendas;
+        console.log(encomendas);
 
         return (
-
-            <Table className='table_in' responsive>
+            <Table className='table_in table-hover' responsive>
                 <thead>
                 <tr>
                     <th>#</th>
                     <th>Data</th>
                     <th>Hora</th>
                     <th>Local</th>
-                    <th>Cliente</th>
                     <th>Temperatura</th>
                     <th>Tamanho</th>
+                    <th>Cliente</th>
                     <th>Estafeta</th>
                     <th>Cacifo</th>
-                    <th>Recolha em</th>
-                    <th>Editar</th>
+                    <th>Recolha</th>
+                    <th>Ações</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <th scope="row">200</th>
-                    <td>01/11/2020</td>
-                    <td>20:30h</td>
-                    <td>Universidade de Aveiro</td>
-                    <td>Sónia Machado</td>
-                    <td>20ºC</td>
-                    <td>Sónia Machado</td>
-                    <td>Universidade de Aveiro</td>
-                    <td>Sónia Machado</td>
-                    <td>Sónia Machado</td>
-                    <td>Editar</td>
 
-                </tr>
-                <tr>
-                    <th scope="row">200</th>
-                    <td>01/11/2020</td>
-                    <td>20:30h</td>
-                    <td>Universidade de Aveiro</td>
-                    <td>Sónia Machado</td>
-                    <td>20ºC</td>
-                    <td>Sónia Machado</td>
-                    <td>Universidade de Aveiro</td>
-                    <td>Sónia Machado</td>
-                    <td>Sónia Machado</td>
-                    <td>Editar</td>
+                {encomendas.map(encomenda => {
 
-                </tr>
-                <tr>
-                    <th scope="row">200</th>
-                    <td>01/11/2020</td>
-                    <td>20:30h</td>
-                    <td>Universidade de Aveiro</td>
-                    <td>Sónia Machado</td>
-                    <td>20ºC</td>
-                    <td>Sónia Machado</td>
-                    <td>Universidade de Aveiro</td>
-                    <td>Sónia Machado</td>
-                    <td>Sónia Machado</td>
-                    <td>Editar</td>
-                </tr>
-                <tr>
-                    <th scope="row">200</th>
-                    <td>01/11/2020</td>
-                    <td>20:30h</td>
-                    <td>Universidade de Aveiro</td>
-                    <td>Sónia Machado</td>
-                    <td>20ºC</td>
-                    <td>Sónia Machado</td>
-                    <td>Universidade de Aveiro</td>
-                    <td>Sónia Machado</td>
-                    <td>Sónia Machado</td>
-                    <td>Editar</td>
-                </tr>
+                    let {id, temperatura, tamanho, localizacao,data_de_entrega,tempo_limite_de_levantamento, data_de_entrega_pretendida,cliente, cacifo} = encomenda;
+
+
+                    const data_entrega = data_de_entrega_pretendida.split(" ");
+
+                    const m = new Date();
+                    const new_timestamp =
+                        m.getUTCFullYear() + "-" +
+                        ("0" + (m.getUTCMonth() + 1)).slice(-2) + "-" +
+                        ("0" + m.getUTCDate()).slice(-2) + " " +
+                        ("0" + m.getUTCHours()).slice(-2) + ":" +
+                        ("0" + m.getUTCMinutes()).slice(-2) + ":" +
+                        ("0" + m.getUTCSeconds()).slice(-2);
+                    const prazo_levantamento = new_timestamp.split(" ");
+
+
+                    const data_split = tempo_limite_de_levantamento.split(" ");
+                    const now = moment(data_split[0]); //todays date
+                    const end = moment(prazo_levantamento[0]); // another date
+                    const duration = moment.duration(now.diff(end));
+                    let days = duration.asDays();
+
+                    if (days <= 0) {
+                        days =
+                            <Alert color="danger">
+                                Prazo excedido
+                            </Alert>
+                    }
+                    else if (days > 0) {
+                        days =
+                            <Alert color="green">
+                                Levantamento em {days} dias
+                            </Alert>
+                    }
+
+
+                    if(data_de_entrega !=null){
+                        return(
+                            <div></div>
+                        );
+                  }
+                  else
+
+                    return (
+                        <tr key={id}>
+                            <th scope="row">{id}</th>
+                            <td>{data_entrega[0]}</td>
+                            <td>{data_entrega[1]}</td>
+                            <td>{localizacao}</td>
+                            <td>{temperatura}ºC</td>
+                            <td>{tamanho}</td>
+                            <td>{cliente.nome}</td>
+                            {encomenda.estafeta.map(estafeta =>
+                                <td>{estafeta.nome}</td>
+                            )}
+                            <td>{cacifo.numero}</td>
+
+                            <td>{days}</td>
+                            <td>
+                                <span className="dropdown">
+				                        <button id="btnSearchDrop2" style={{backgroundColor: '#b5a0fb', border: 'none', width:'68px'}}
+                                                type="button" data-toggle="dropdown" aria-haspopup="true"
+                                                aria-expanded="false"
+                                                className="btn btn-dark dropdown-toggle dropdown-menu-right">
+                                            <i className="material-icons md-18" style={{
+                                                color: 'white',
+                                                verticalAlign: 'middle',
+                                                marginRight: '5px'
+                                            }}>settings</i>
+                                        </button>
+				                        <span aria-labelledby="btnSearchDrop2"
+                                              className="btn_acoes dropdown-menu mt-1 dropdown-menu-right">
+				                            <Link to={{pathname: `detail/${id}`, query: {id: id}}} className="dropdown-item"> <i className="material-icons md-18 icon">remove_red_eye</i> Abrir</Link>
+				                            <Link to="#" className="dropdown-item"><i className="material-icons md-18 icon">create</i> Editar</Link>
+				                            <Link to="#" className="dropdown-item"><i className="material-icons md-18 icon">delete</i> Remover</Link>
+				                        </span>
+				                    </span>
+                            </td>
+
+                        </tr>
+
+                    );
+
+
+                })}
+
                 </tbody>
             </Table>
         );
     }
 }
-
