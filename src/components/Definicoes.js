fff19f3865435b7c71c4cf6541a462e09165f7a5
Francisco <franciscocunha156@gmail.com>
2019-02-04 18:49:32 +0000

    definiçoes

--- src/components/Definicoes.js
+++ src/components/Definicoes.js
@@ -1,145 +1,132 @@
-import React from 'react';
-import  {Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import React, {Component} from 'react';
+import {Form, FormGroup, Label, Input, FormText, Button} from 'reactstrap';
+import {Link} from "react-router-dom";
+import axios from "axios";
+
 
 export default class Definicoes extends React.Component {
+
+
+    state = {
+        userinfo: []
+    };
+
+    componentDidMount() {
+        axios.get('http://167.99.202.225/api/users/1')
+            .then(response => {
+                this.setState({userinfo: response.data.data});
+                console.log(this.state.userinfo.tipo.tipo);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+
+
+
+
     render() {
+
+        const {userinfo} = this.state;
+
         return (
-            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
-            <Form>
-                <FormGroup>
-                    <Label for="exampleEmail">Plain Text (Static)</Label>
-                    <Input plaintext value="Some plain text/ static value" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
+            <main style={{zIndex: '-15555px'}} role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+                <div
+                    className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+                    <h1 className="h2">Definições</h1>
+                </div>
+            <div style={{margin:'0 auto' , height:'auto'}}>
+                <div style={{margin:'0 auto 100px auto' , width:'70%'}}>
+
+                </div>
+                <div style={{margin:'0 auto 20px auto' , width:'70%'}}>
+                    <Label for="nome">Nome Completo</Label>
+                    <Input
+                        type="nome"
+                        name="nome"
+                        id="nome"
+                        placeholder={this.state.userinfo.nome}
+                     />
+                </div>
+                <div style={{margin:'0 auto 20px auto' , width:'70%'}}>
+                    <Label for="email">Email</Label>
                     <Input
                         type="email"
                         name="email"
-                        id="exampleEmail"
-                        placeholder="with a placeholder"
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="examplePassword">Password</Label>
+                        id="email"
+                        placeholder={this.state.userinfo.email}
+                        />
+
+                </div>
+                <div style={{margin:'0 auto 20px auto' , width:'70%'}}>
+                    <Label for="telefone">Telefone</Label>
                     <Input
-                        type="password"
-                        name="password"
-                        id="examplePassword"
-                        placeholder="password placeholder"
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleUrl">Url</Label>
+                        type="telefone"
+                        name="telefone"
+                        id="telefone"
+                        placeholder={this.state.userinfo.telefone}
+                        />
+
+                </div>
+                <div style={{margin:'0 auto 20px auto' , width:'70%'}}>
+                    <Label for="data_nascimento">Data de nascimento</Label>
                     <Input
-                        type="url"
-                        name="url"
-                        id="exampleUrl"
-                        placeholder="url placeholder"
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleNumber">Number</Label>
+                        type="data_nascimento"
+                        name="data_nascimento"
+                        id="data_nascimento"
+                        placeholder={this.state.userinfo.data_nascimento}
+                        />
+
+                </div>
+                <div style={{margin:'0 auto 20px auto' , width:'70%'}}>
+
+                    <form>
+                        <Label for="tipo">Password</Label>
+                        <div className="form-row">
+                            <div className="col" style={{display:'inline' , float:'left'}}>
+                                <input disabled style ={{outline:'none' , border:'none'}}type="text" className="form-control" placeholder="***********"></input>
+                            </div>
+                            <div style={{display:'inline' , float:'right'}} >
+                                <Button color="link"> <Link to={{}}
+
+                                >Alterar password</Link>
+                                </Button>
+
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+                <div style={{margin:'0 auto 20px auto' , width:'70%'}}>
+                    <div style={{marginBottom: '20px '}}>
+                    <Label for="tipo">Função</Label>
                     <Input
-                        type="number"
-                        name="number"
-                        id="exampleNumber"
-                        placeholder="number placeholder"
+                        type="tipo"
+                        name="tipo"
+                        id="tipo"
+                        placeholder={this.state.userinfo.tipo}
                     />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleDatetime">Datetime</Label>
-                    <Input
-                        type="datetime"
-                        name="datetime"
-                        id="exampleDatetime"
-                        placeholder="datetime placeholder"
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleDate">Date</Label>
-                    <Input
-                        type="date"
-                        name="date"
-                        id="exampleDate"
-                        placeholder="date placeholder"
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleTime">Time</Label>
-                    <Input
-                        type="time"
-                        name="time"
-                        id="exampleTime"
-                        placeholder="time placeholder"
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleColor">Color</Label>
-                    <Input
-                        type="color"
-                        name="color"
-                        id="exampleColor"
-                        placeholder="color placeholder"
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleSearch">Search</Label>
-                    <Input
-                        type="search"
-                        name="search"
-                        id="exampleSearch"
-                        placeholder="search placeholder"
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleSelect">Select</Label>
-                    <Input type="select" name="select" id="exampleSelect">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleSelectMulti">Select Multiple</Label>
-                    <Input
-                        type="select"
-                        name="selectMulti"
-                        id="exampleSelectMulti"
-                        multiple
-                    >
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleText">Text Area</Label>
-                    <Input type="textarea" name="text" id="exampleText" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleFile">File</Label>
-                    <Input type="file" name="file" id="exampleFile" />
-                    <FormText color="muted">
-                        This is some placeholder block-level help text for the above input.
-                        It's a bit lighter and easily wraps to a new line.
-                    </FormText>
-                </FormGroup>
-                <FormGroup check>
-                    <Label check>
-                        <Input type="radio" /> Option one is this and that—be sure to
-                        include why it's great
-                    </Label>
-                </FormGroup>
-                <FormGroup check>
-                    <Label check>
-                        <Input type="checkbox" /> Check me out
-                    </Label>
-                </FormGroup>
-            </Form>
+                        </div>
+                    <Button style={{padding: '0'}}color="link"> <Link to={{}}
+
+                    >Apagar conta</Link>
+                    </Button>
+
+                    <div style={{margin:'0 auto 20px auto'}}>
+                    <Button style={{marginRight:'20px'}} className='btn_detalhes' color="link" size="m" ><Link to="/"
+
+                    >Cancelar</Link></Button>
+
+
+                    <Button className='btn_detalhes' size="m"> <Link to={{}}
+
+                    >Guardar alterações</Link>
+                    </Button>
+                    </div>
+                </div>
+
+            </div>
             </main>
         );
     }
