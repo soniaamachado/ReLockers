f4db6f2927e5aae7a757092e641d10c13dde57a1
Bruno Rafael Graça Gameiro <brunogameiro@ua.pt>
2019-02-06 00:48:36 +0000

    inicio redux

--- src/App.js
+++ src/App.js
@@ -14,32 +14,34 @@ import DetalhesCacifo from "./components/DetalhesCacifo";
 import AdicionarEncomenda from "./components/AdicionarEncomenda";
 import ApagarEncomenda from "./components/ApagarEncomenda";
 
+import {Provider} from "react-redux";
+import store from './store';
 
 
 class App extends Component {
     render() {
         return (
-            <Router>
-                <Switch>
-                    <div>
-                        <Navbar/>
-                        <Route exact path='/encomendas' component={Encomendas}/>
-                        <Route exact path='/encomendas/entregues' component={EncomendasEntregues}/>
-                        <Route exact path='/estafetas' component={Estafetas}/>
-                        <Route exact path='/cacifos' component={Cacifos}/>
-                        <Route exact path='/definicoes' component={Definicoes}/>
-                        <Route exact path='/detail/:id' component={DetalhesEncomenda}/>
-                        <Route exact path='/detalheEstafeta/:id' component={DetalhesEstafeta}/>
-                        <Route exact path='/detalheCacifo/:id' component={DetalhesCacifo}/>
-                        <Route exact path='/detalheEncomenda/:id' component={DetalhesEncomenda}/>
-                        <Route exact path='/' component={Inicio}/>
-                        <Route exact path='/adicionar_encomenda' component={AdicionarEncomenda}/>
-                        <Route exact path='/apagarEncomenda/:id' component={ApagarEncomenda}/>
-
-
-                    </div>
-                </Switch>
-            </Router>
+            <Provider store={store}>
+                <Router>
+                    <Switch>
+                        <div>
+                            <Navbar/>
+                            <Route exact path='/encomendas' component={Encomendas}/>
+                            <Route exact path='/encomendas/entregues' component={EncomendasEntregues}/>
+                            <Route exact path='/estafetas' component={Estafetas}/>
+                            <Route exact path='/cacifos' component={Cacifos}/>
+                            <Route exact path='/definicoes' component={Definicoes}/>
+                            <Route exact path='/detail/:id' component={DetalhesEncomenda}/>
+                            <Route exact path='/detalheEstafeta/:id' component={DetalhesEstafeta}/>
+                            <Route exact path='/detalheCacifo/:id' component={DetalhesCacifo}/>
+                            <Route exact path='/detalheEncomenda/:id' component={DetalhesEncomenda}/>
+                            <Route exact path='/' component={Inicio}/>
+                            <Route exact path='/adicionar_encomenda' component={AdicionarEncomenda}/>
+                            <Route exact path='/apagarEncomenda/:id' component={ApagarEncomenda}/>
+                        </div>
+                    </Switch>
+                </Router>
+            </Provider>
         );
     }
 }
