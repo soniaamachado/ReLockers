793b06a7db09c98f1336da00c2da41378dd9ea50
Caio Jacobina <caiosantanaj@gmail.com>
2019-02-07 17:26:51 +0000

    revert sonia commit

--- src/App.js
+++ src/App.js
@@ -11,9 +11,8 @@ import DetalhesEncomenda from "./components/DetalhesEncomenda";
 import DetalhesEstafeta from "./components/DetalhesEstafeta";
 import DetalhesCacifo from "./components/DetalhesCacifo";
 import AdicionarEncomenda from "./components/AdicionarEncomenda";
-import ApagarEncomenda from "./components/ApagarEncomenda";
-import { Provider } from "react-redux";
-import store from './store';
+import Login from "./components/Login";
+import ApagarEncomenda from "./components/ApagarEncomenda"
 import AdicionarEstafeta from './components/AdicionarEstafeta';
 import ApagarEstafeta from "./components/ApagarEstafeta";
 import Confirmacao from "./components/Confirmacao";
@@ -22,29 +21,28 @@ import Confirmacao from "./components/Confirmacao";
 class App extends Component {
     render() {
         return (
-            <Provider store={store}>
-                <Router>
-                    <Switch>
-                        <div>
-                            <Navbar />
-                            <Route exact path='/encomendas' component={Encomendas} />
-                            <Route exact path='/estafetas' component={Estafetas} />
-                            <Route exact path='/cacifos' component={Cacifos} />
-                            <Route exact path='/definicoes' component={Definicoes} />
-                            <Route exact path='/detail/:id' component={DetalhesEncomenda} />
-                            <Route exact path='/detalheEstafeta/:id' component={DetalhesEstafeta} />
-                            <Route exact path='/detalheCacifo/:id' component={DetalhesCacifo} />
-                            <Route exact path='/detalheEncomenda/:id' component={DetalhesEncomenda} />
-                            <Route exact path='/' component={Inicio} />
-                            <Route exact path='/adicionar_encomenda' component={AdicionarEncomenda} />
-                            <Route exact path='/adicionar_estafeta' component={AdicionarEstafeta} />
-                            <Route exact path='/apagarEncomenda/:id' component={ApagarEncomenda} />
-                            <Route exact path='/apagarEstafeta/:id' component={ApagarEstafeta} />
-                            <Route exact path='/confirmacao' component={Confirmacao} />
-                        </div>
-                    </Switch>
-                </Router>
-            </Provider>
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={Login} />
+                    <div>
+                        <Navbar />
+                        <Route exact path='/encomendas' component={Encomendas} />
+                        <Route exact path='/estafetas' component={Estafetas} />
+                        <Route exact path='/cacifos' component={Cacifos} />
+                        <Route exact path='/definicoes' component={Definicoes} />
+                        <Route exact path='/detail/:id' component={DetalhesEncomenda} />
+                        <Route exact path='/detalheEstafeta/:id' component={DetalhesEstafeta} />
+                        <Route exact path='/detalheCacifo/:id' component={DetalhesCacifo} />
+                        <Route exact path='/detalheEncomenda/:id' component={DetalhesEncomenda} />
+                        <Route exact path='/inicio' component={Inicio} />
+                        <Route exact path='/adicionar_encomenda' component={AdicionarEncomenda} />
+                        <Route exact path='/adicionar_estafeta' component={AdicionarEstafeta} />
+                        <Route exact path='/apagarEncomenda/:id' component={ApagarEncomenda} />
+                        <Route exact path='/apagarEstafeta/:id' component={ApagarEstafeta} />
+                        <Route exact path='/confirmacao' component={Confirmacao} />
+                    </div>
+                </Switch>
+            </Router>
         );
     }
 }
